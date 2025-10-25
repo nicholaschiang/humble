6fef4f9d1e0b515d1cb4a863ea4f1b92dd14f321
Matthew Lund <lundmatthewj@gmail.com>
2025-10-25 14:06:27 -0600

    feat: add email/password login flow

---
+++ components/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Welcome = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", padding: 16 }}>
+      <Text>You are signed in.</Text>
+    </View>
+  );
+};
+
+export default Welcome;

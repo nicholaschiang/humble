88bb2fea2bce628fc9aa6d9f4f33054725d3a087
Matthew Lund <lundmatthewj@gmail.com>
2025-10-25 14:06:27 -0600

    Initial login logic

---
+++ service/AuthService.ts
@@ -0,0 +1,36 @@
+import { supabase } from '../lib/supabase';
+
+export const loginWithEmail = async (email: string, password: string) => {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+  if (error) {
+    console.error('Error signing in:', error);
+    throw error;
+  }
+  console.log('Signed in!');
+  return data;
+};
+
+export const registerWithEmail = async (email: string, password: string) => {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+  if (error) {
+    console.error('Error signing up:', error);
+    throw error;
+  }
+  console.log('Signed up!');
+  return data;
+};
+
+export const logout = async () => {
+  const { error } = await supabase.auth.signOut();
+  if (error) {
+    console.error('Error signing out:', error);
+    throw error;
+  }
+  console.log('Signed out!');
+}

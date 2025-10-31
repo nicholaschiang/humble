78f9be7f6ceb38d864604c4b3c09bd4532f6f8b1
Matthew Lund <lundmatthewj@gmail.com>
2025-10-31 11:03:30 -0600

    feat: style auth and welcome

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,12 +1,28 @@
+import theme from '@/constants/theme';
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const Welcome = () => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", padding: 16 }}>
-      <Text>You are signed in.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>You are signed in.</Text>
     </View>
   );
 };
 
 export default Welcome;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: theme.spacing.md,
+    backgroundColor: theme.colors.background,
+  },
+  text: {
+    color: theme.colors.text,
+    fontSize: theme.fonts.sizes.lg,
+    fontFamily: theme.fonts.fontFamily,
+  },
+});

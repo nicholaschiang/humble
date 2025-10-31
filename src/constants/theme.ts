63dceeaa1a845b83586193fe99d0870d42f122a5
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2025-10-31 02:53:56 -0600

    feat(constants/theme): use black background color

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -11,15 +11,15 @@ const tintColorDark = "#fff";
 export const Colors = {
   light: {
     text: "#11181C",
-    background: "#fff",
+    background: "#FFFFFF",
     tint: tintColorLight,
     icon: "#687076",
     tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: "#ECEDEE",
-    background: "#151718",
+    text: "#FFFFFF",
+    background: "#000000",
     tint: tintColorDark,
     icon: "#9BA1A6",
     tabIconDefault: "#9BA1A6",

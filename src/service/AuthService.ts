348af66a64a13d16facbabd90a4b45c175b5ffb4
Matthew Lund <lundmatthewj@gmail.com>
2025-11-03 22:29:10 -0700

    feat: create single AuthForm to handle auth

---
+++ src/service/AuthService.ts
@@ -0,0 +1,54 @@
+import { AuthError } from "@/errors";
+import { supabase } from "@/lib/supabase";
+
+async function createProfile(
+  userId: string,
+  username: string,
+  firstName?: string,
+  lastName?: string,
+) {
+  const { error } = await supabase.from("Profile").insert({
+    user_id: userId,
+    username: username,
+    first_name: firstName,
+    last_name: lastName,
+  });
+  if (error) {
+    throw new AuthError(error.message); // Throw the error instead of showing an alert
+  }
+  console.log("Profile created!");
+}
+
+export async function signIn(email: string, password: string) {
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) {
+    throw new AuthError(error.message); // Pass the error to the caller
+  }
+}
+
+export async function register(
+  email: string,
+  password: string,
+  username: string,
+  first_name?: string,
+  last_name?: string,
+) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+  if (error) {
+    throw new AuthError(error.message); // Pass the error to the caller
+  }
+  await createProfile(data.user!.id, username, first_name, last_name);
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) {
+    throw new AuthError(error.message); // Pass the error to the caller
+  }
+}

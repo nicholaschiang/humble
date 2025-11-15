7841553f45129a12ff65774e3512717a050ea68b
Matthew Lund <lundmatthewj@gmail.com>
2025-11-15 14:29:01 -0700

    feat: make FormContainer keyboard avoidant

--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -1,13 +1,29 @@
 import { type PropsWithChildren } from "react";
 import { Label } from "@/components/ui/label";
-import { View } from "react-native";
+import { KeyboardAvoidingView, Platform, ScrollView, View } from "react-native";
 
 export function FormContainer(props: PropsWithChildren) {
   return (
-    <View
-      className="flex flex-col gap-4 h-full w-full justify-center p-4 bg-background max-w-lg mx-auto"
-      {...props}
-    />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+          justifyContent: "center",
+          paddingHorizontal: 16,
+          paddingBottom: 64,
+          paddingTop: 100,
+        }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <View
+          className="flex flex-col gap-4 w-full bg-background max-w-lg mx-auto"
+          {...props}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 

9dda3b6767e5aa2b42475b054ba5dc90dc2a2b02
Matthew Lund <lundmatthewj@gmail.com>
2025-11-15 14:58:34 -0700

    fix: linter

--- service/AuthService.ts
+++ service/AuthService.ts
@@ -1,4 +1,4 @@
-import { supabase } from '../lib/supabase';
+import { supabase } from "../lib/supabase";
 
 export const registerWithEmail = async (email: string, password: string) => {
   const { data, error } = await supabase.auth.signUp({
@@ -6,31 +6,31 @@ export const registerWithEmail = async (email: string, password: string) => {
     password,
   });
   if (error) {
-    console.error('Error signing up:', error);
+    console.error("Error signing up:", error);
     throw error;
   }
-  console.log('Signed up!');
+  console.log("Signed up!");
   return data;
 };
 
 export const loginWithEmail = async (email: string, password: string) => {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
-    password
+    password,
   });
   if (error) {
-    console.error('Error signing in:', error);
+    console.error("Error signing in:", error);
     throw error;
   }
-  console.log('Signed in!');
+  console.log("Signed in!");
   return data;
 };
 
 export const logout = async () => {
   const { error } = await supabase.auth.signOut();
   if (error) {
-    console.error('Error signing out:', error);
+    console.error("Error signing out:", error);
     throw error;
   }
-  console.log('Signed out!');
-}
+  console.log("Signed out!");
+};

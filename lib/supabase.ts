a8eb0470abd336fbbe5e1f31f60ead6d12bdb68a
Matthew Lund <lundmatthewj@gmail.com>
2025-11-15 14:58:34 -0700

    fix: linter

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-    process.env.EXPO_PUBLIC_SUPABASE_URL!,
-    process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!,
+  process.env.EXPO_PUBLIC_SUPABASE_URL!,
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!,
 );
-

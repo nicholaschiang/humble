8230e3fee07c2b99f1b9e249226872551e6facdb
Matthew Lund <lundmatthewj@gmail.com>
2025-11-16 18:06:05 -0700

    feat: add feed to home screen

--- src/components/history/UserFeed.tsx
+++ src/components/history/UserFeed.tsx
@@ -2,7 +2,5 @@ import { getGymVisitFeedForUser } from "@/service/WorkoutService";
 import { GymVisitHistory } from "./GymVisitHistory";
 
 export function UserFeed({ userId }: { userId: string }) {
-  return (
-    <GymVisitHistory getGymVisits={() => getGymVisitFeedForUser(userId)} />
-  );
+  return <GymVisitHistory getGymVisits={() => getGymVisitFeedForUser(userId)} />;
 }

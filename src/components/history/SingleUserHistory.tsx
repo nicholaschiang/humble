513afe1b867cee4f6dbb0d8a6cc6ff9d595a0b53
Matthew Lund <lundmatthewj@gmail.com>
2025-11-16 18:14:17 -0700

    feat: style the cards a bit nicer

--- src/components/history/SingleUserHistory.tsx
+++ src/components/history/SingleUserHistory.tsx
@@ -2,5 +2,8 @@ import { getGymVisitsByUser } from "@/service/WorkoutService";
 import { GymVisitHistory } from "./GymVisitHistory";
 
 export function SingleUserHistory({ userId }: { userId: string }) {
-  return <GymVisitHistory getGymVisits={() => getGymVisitsByUser(userId)} />;
+  return <GymVisitHistory
+    getGymVisits={() => getGymVisitsByUser(userId)}
+    title="User History"
+  />;
 }
